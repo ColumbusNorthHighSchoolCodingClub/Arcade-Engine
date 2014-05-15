0ce294dcb14ad97fa732554d63be736205631bac
thatapplefreak <thatapplefreak@gmail.com>
2014-05-15 13:27:16 -0400

    Skeleton of PongDemo

--- ArcadeEngineDemo/java/gui/pong/GuiPong.java
+++ ArcadeEngineDemo/java/gui/pong/GuiPong.java
@@ -10,22 +10,33 @@ import com.arcadeengine.gui.Gui;
 import com.arcadeengine.gui.GuiButton;
 import com.arcadeengine.sound.SoundHandler;
 
-public class GuiPong extends Gui implements Pausable {	
+public class GuiPong extends Gui implements Pausable {
+	
+	private Paddle leftPaddle, rightPaddle;
+	private Ball ball;
 	
 	public GuiPong(AnimPanel panel) {
 		super(panel);
 		setBGColor(Color.BLACK);
 		SoundHandler.getInstance().pause();
+		
+		ball = new Ball(panel);
+		leftPaddle = new Paddle(panel, true);
+		rightPaddle = new Paddle(panel, false);
 	}
 
 	@Override
 	public void drawGui(Graphics g) {
-		
+		leftPaddle.draw(g);
+		rightPaddle.draw(g);
+		ball.draw(g);
 	}
 
 	@Override
 	public void updateGui() {
-		
+		leftPaddle.think();
+		rightPaddle.think();
+		ball.think();
 	}
 
 	@Override
